3d162b9358377c568b0a1a441d39b3b98ac0c7e1
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-11-13 16:36:48 +0100

    almost good

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :content, :rating, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 0..5 }
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: {
+  only_integer: true,
+  greater_than_or_equal_to: 0,
+  less_than_or_equal_to: 5,
+  message: "must be a number between 0 and 5"
+}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# puts"Creating 5 fake restaurants"
+# 5.times do
+#   restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+#   restaurant.save!
+# end
+# puts"finished"
+
+puts"Creating 5 fake restaurants"
+5.times do
+  restaurant = Restaurant.new(name: "La Tour d'Argent", address: "15 Quai de la Tournelle, 75005 Paris", phone_number: "01 43 54 23 31", category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save
+end
+puts"finished"
